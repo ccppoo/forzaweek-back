54dbed256148b4b167f6a4cb0c0808fec0b6ee9b
ccppoo <shmishmi79@gmail.com>
2024-08-07 22:34:15 +0900

    update board image post api

--- app/routers/image.py
+++ app/routers/image.py
@@ -2,22 +2,19 @@ from __future__ import annotations
 import boto3.s3
 import boto3.session
 import botocore.client
-from fastapi import APIRouter, UploadFile, File, Query
-from fastapi.websockets import WebSocketState
+from fastapi import APIRouter, UploadFile, File, Query, Depends, Body
 from pydantic import BaseModel, Field
-import anyio
 from typing import List, Dict, Any, Optional, Annotated
-import asyncio
-import json
 from pprint import pprint
 from datetime import datetime
 import boto3
 from app.configs import awsSettings, runtimeSettings, cfSettings
 import uuid
 from app.services.image import resolve_temp_image, delete_uploade_image
+from app.services.auth.deps import get_current_active_user
+from app.models.user import UserAuth
 
 import botocore
-import urllib
 import pathlib
 
 
@@ -72,26 +69,19 @@ async def add_nation_flag(folder: str, file: UploadFile):
     return {"image": temp_file_name}
 
 
-@router.delete("/board/upload")
-async def upload_image_from_board(delete: Annotated[str, Query()]):
-    # TODO: 파일 업로드하고 사용 안된 임시파일 삭제하기
-
-    cfURL = delete_uploade_image(delete)
-    # 임시 저장한 파일 이름 다시 보내기 (어차피 경로는 POST : /nation으로 오니깐)
-    # print(f"temp_file_name : {temp_file_name}")
-    return {
-        "success": 1,
-        "file": {
-            "url": cfURL,
-        },
-    }
-    # return {"image": temp_file_name}
+class BoardImageDeleteRequest(BaseModel):
+    url: str  # url like https://cdn.forzaweek.***
 
 
+# https://fzwcdn.forzaweek.com/board/board/test/d7546921-188e-474e-bce8-4f872dfcec76.jpg
 @router.post("/board/upload")
-async def upload_image_from_board(file: UploadFile):
+async def upload_image_from_board(
+    current_user: Annotated[UserAuth, Depends(get_current_active_user)],
+    file: UploadFile,
+):
     # TODO: 파일 업로드하고 사용 안된 임시파일 삭제하기
 
+    print(f"current_user : {current_user.oauth.xbox.gamer_tag}")
     temp_image_allow = [
         "board",
     ]
@@ -111,9 +101,35 @@ async def upload_image_from_board(file: UploadFile):
 
     random_name = str(uuid.uuid4())
 
-    cfURL = resolve_temp_image("board", temp_file_name, random_name, "board", "test")
+    cfURL = resolve_temp_image(
+        "board", temp_file_name, random_name, current_user, "board", "test"
+    )
     # 임시 저장한 파일 이름 다시 보내기 (어차피 경로는 POST : /nation으로 오니깐)
     # print(f"temp_file_name : {temp_file_name}")
+    return {
+        "success": 1,
+        "file": {
+            "url": cfURL,
+        },
+    }
+    # return {"image": temp_file_name}
+
+
+@router.post("/board/delete")
+async def delete_image_from_board(
+    current_user: Annotated[UserAuth, Depends(get_current_active_user)],
+    delete_request: Annotated[BoardImageDeleteRequest, Body()],
+):
+    # TODO: 파일 업로드하고 사용 안된 임시파일 삭제하기
+    print(f"current_user : {current_user.oauth.xbox.gamer_tag}")
+
+    pprint(delete_request)
+    # 1. 삭제 요청을 보내고 CF 에서 HEAD 요청 받아옴 -> 검증 -> 샂게
+    # 1. 삭제 요청을
+    cfURL = delete_uploade_image(delete_request.url, current_user)
+    # 임시 저장한 파일 이름 다시 보내기 (어차피 경로는 POST : /nation으로 오니깐)
+    # print(f"temp_file_name : {temp_file_name}")
+    return
     return {
         "success": 1,
         "file": {
--- app/services/image.py
+++ app/services/image.py
@@ -4,11 +4,16 @@ from app.configs import runtimeSettings, cfSettings
 from typing import Tuple, Union
 from app.types.http import Url
 from app.cloud import client_r2
+from app.models.user import UserAuth
+from pydantic import BaseModel
+from .temp import R2_object_head_response, R2_ObjectMetaData, R2_object_delete_response
+
+from pprint import pprint
 
 
 # FIXME: async 병렬 or 병렬
 def resolve_temp_image(
-    folder: str, temp_image: str, new_name: str, *paths: Tuple[str]
+    folder: str, temp_image: str, new_name: str, user: UserAuth, *paths: Tuple[str]
 ) -> Union[Url, None]:
 
     base_dir = runtimeSettings.TEMPFILE_BASE_DIR
@@ -35,13 +40,9 @@ def resolve_temp_image(
     if fname_temp.suffix.endswith("jpg"):
         CONTENT_TYPE = "jpg"
     # https://fzwcdn.forzaweek.com/board/board/test/1eac1219-bb82-4c47-b88e-9b5cc757d3fd.jpg
-    # ACL, CacheControl, ChecksumAlgorithm, ContentDisposition, ContentEncoding,
-    # ContentLanguage, ContentType, ExpectedBucketOwner, Expires,
-    # GrantFullControl, GrantRead, GrantReadACP, GrantWriteACP, Metadata,
-    # ObjectLockLegalHoldStatus, ObjectLockMode, ObjectLockRetainUntilDate,
-    # RequestPayer, ServerSideEncryption, StorageClass, SSECustomerAlgorithm,
-    # SSECustomerKey, SSECustomerKeyMD5, SSEKMSKeyId, SSEKMSEncryptionContext, Tagging, WebsiteRedirectLocation
 
+    # TODO
+    metadata = R2_ObjectMetaData(user_id=user.user_id, user_doc_id=str(user.id))
     """
     MetaData : 이미지 메타데이타에 업로드한 사용자 노출용 UID, Document ID 첨부해서
     삭제할 때 JWT로 1차 검증하고, 삭제할 때 
@@ -52,10 +53,7 @@ def resolve_temp_image(
         Key=resolved_key,
         ExtraArgs={
             "ContentType": f"image/{CONTENT_TYPE}",
-            "Metadata": {
-                "upload_user_id": "123",
-                "upload_user_doc_id": "aaabbbccc",
-            },
+            "Metadata": metadata.model_dump(),
         },
     )
 
@@ -68,28 +66,29 @@ def resolve_temp_image(
 
 
 # FIXME: async 병렬 or 병렬
-def delete_uploade_image(originImageURL: str) -> Union[Url, None]:
+def delete_uploade_image(originImageURL: str, user: UserAuth) -> Union[Url, None]:
 
     uploaded_file_key = originImageURL.replace(cfSettings.URL_BASE, "")[1:]
     # NOTE: 그냥 R2 worker 만들어서 할까..?
-    # client_r2.download_file
-    # attr = client_r2.get_object_attributes(
-    #     Bucket=cfSettings.BUCKET, Key=uploaded_file_key, ObjectAttributes=["ETag"]
-    # )
-    print()
 
+    print()
     print(f"{originImageURL=}")
     print()
     print(f"{uploaded_file_key=}")
     print()
-    from pprint import pprint
 
     head = client_r2.head_object(Bucket=cfSettings.BUCKET, Key=uploaded_file_key)
-    head[
-        "Metadata"
-    ]  # 여기서 업로드한 사용자 public user_id + document(private) user_id 검증해서 삭제하면 됨
-    pprint(head)
+    # 여기서 업로드한 사용자 public user_id + document(private) user_id 검증해서 삭제하면 됨
+    head_resposne = R2_object_head_response(**head)
+    # should be
+    match_user_id = head_resposne.Metadata.user_id == user.user_id
+    match_user_oid = head_resposne.Metadata.user_doc_id == str(user.id)
 
-    # client_r2.delete_object(Bucket=cfSettings.BUCKET, Key=uploaded_file_key)
+    if match_user_id and match_user_oid:
+        reply = client_r2.delete_object(Bucket=cfSettings.BUCKET, Key=uploaded_file_key)
+        delete_response = R2_object_delete_response(**reply)
+
+    # 사용자 정보 일치하지 않는 경우 내가 알아서 처리해야함
+    # delete_response.is_success (HTTP code 204 반환)
 
     return
