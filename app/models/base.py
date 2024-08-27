f01c04b565c2e389a3479132d63abed80730cefa
ccppoo <shmishmi79@gmail.com>
2024-08-27 20:26:36 +0900

    a

--- app/models/base.py
+++ app/models/base.py
@@ -1,6 +1,22 @@
+from datetime import datetime
+from typing import List, Optional, Union
+from beanie import Document, Link
 from pydantic import Field, BaseModel
-from app.types import GAME
+from app.utils.time import datetime_utc
+from pprint import pprint
+
+from beanie.odm.actions import after_event
+from beanie.odm.actions import Insert, Save, Replace, Update, SaveChanges
+
+__all__ = ("DecalBase", "DecalName")
 
 
-class GameBase(BaseModel):
-    game: GAME
+class DocumentBase(Document):
+    """DocumentBase"""
+
+    uploaded_at: datetime = Field(default_factory=datetime_utc)
+    last_edited: Optional[datetime] = Field(default=None)
+
+    @after_event(Replace, Update, SaveChanges)
+    def update_last_edit(self):
+        self.last_edited = datetime_utc()
--- app/models/track/traits/fh5/__init__.py
+++
@@ -1,9 +0,0 @@
-from .track_format import TrackFormat
-from .track_category import TrackCategory
-from .track_fullpathImage import TrackFullPathImage
-
-__all__ = (
-    "TrackFullPathImage",
-    "TrackCategory",
-    "TrackFormat",
-)
