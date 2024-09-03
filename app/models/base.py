958024015a50832992714bcbff3432ed44e4f392
ccppoo <shmishmi79@gmail.com>
2024-09-03 23:39:13 +0900

    Update base.py

--- app/models/base.py
+++ app/models/base.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from typing import List, Optional, Union
 from beanie import Document, Link
-from pydantic import Field, BaseModel
+from pydantic import Field
 from app.utils.time import datetime_utc
 from pprint import pprint
 
@@ -17,6 +17,13 @@ class DocumentBase(Document):
     uploaded_at: datetime = Field(default_factory=datetime_utc)
     last_edited: Optional[datetime] = Field(default=None)
 
+    @property
+    def id_str(self) -> str:
+        return str(self.id)
+
     @after_event(Replace, Update, SaveChanges)
     def update_last_edit(self):
         self.last_edited = datetime_utc()
+
+    class Settings:
+        pass
